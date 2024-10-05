01b121b514bfc9f8c88f6a2263f95eea6b72e5de
dyxgou <diego5614yt@gmail.com>
2024-10-05 16:45:41 -0500

    Added Colors

--- src/store/color.ts
+++ src/store/color.ts
@@ -1,6 +1,29 @@
 import { atom } from "nanostores";
 
-export const $color = atom("blue");
+export const COLORS: string[] = [
+  // "black",
+  "#000000",
+  // "white",
+  "#ffffff",
+  // blue
+  "#2653d1",
+  // "green",
+  "#2cdb32",
+  // "red",
+  "#d90909",
+  // "yellow",
+  "#ffc800",
+  // "violet",
+  "#5d00ff",
+  // "gray",
+  "#474747",
+  // "orange",
+  "#fa5300",
+  // "cyan",
+  "#00f7ff",
+];
+
+export const $color = atom(COLORS[0]);
 export const setColor = (color: string) => {
   $color.set(color);
 };
