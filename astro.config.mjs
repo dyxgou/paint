32c5f1b163e2878ffb9f2ad41313357cda610b02
dyxgou <diego5614yt@gmail.com>
2024-10-12 15:52:43 -0500

    feat: Config files to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: vercel()
 });
